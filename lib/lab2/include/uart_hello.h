a2bdc1ec14fef65d07216f8d873ab0eb09bcc07f
CARLOS CARVAJAL <u1424676@lab1-17.eng.utah.edu>
2023-09-11 16:04:02 -0600

    implemented uart tests

--- lib/lab2/include/uart_hello.h
+++ lib/lab2/include/uart_hello.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #ifdef TESTING_ENV
+    test_uart_in();
+    test_uart_out();
 struct device {};
 #else
+    uart_poll_in();
+    uart_poll_out();
 #include <drivers/uart.h>
 #endif
 
---
+++ test/uart_test/uart_map.c
@@ -0,0 +1,14 @@
+#include "map.h"
+
+
+#define TESTING_ENV 1
+
+test_uart_in()
+{
+
+}
+
+test_uart_out()
+{
+
+}
